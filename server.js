9b8cab68b6e34282c767f2a4104577fb03b4c259
Trinadh Karanam <tkaranam@cmhc-schl.gc.ca>
2026-04-02 17:08:58 +0530

    added root route

--- server.js
+++ server.js
@@ -86,3 +86,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
