3503ed8047265dc1131bba96824e15fea9c8a996
Trinadh Karanam <tkaranam@cmhc-schl.gc.ca>
2026-04-02 17:45:18 +0530

    added db configs

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
@@ -8,10 +9,11 @@ app.use(express.json());
 
 // DB connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "radeck",
-  database: "usersDB"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
